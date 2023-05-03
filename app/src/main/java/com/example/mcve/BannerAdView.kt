223368919cb7c8c56100c73d47848ae7a3108506
cristhian <cristhian.espinosa@dowjones.com>
2023-05-03 10:13:58 -0500

    try google solution

--- app/src/main/java/com/example/mcve/BannerAdView.kt
+++ app/src/main/java/com/example/mcve/BannerAdView.kt
@@ -3,6 +3,7 @@ package com.example.mcve
 import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.doOnLayout
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.admanager.AdManagerAdView
@@ -12,7 +13,7 @@ import com.google.android.gms.ads.admanager.AdManagerAdView
 fun BannerAdView(
     isTest: Boolean = false
 ) {
-    val unitId = "ca-app-pub-3940256099942544/6300978111"
+    val unitId = "/2/androidphone.us.wsj.app/collection"
 
     AndroidView(
         factory = { context ->
@@ -21,8 +22,12 @@ fun BannerAdView(
                     AdSize(AdType.CUBE.width, AdType.CUBE.height),
                     AdSize(AdType.BIG_CUBE.width, AdType.BIG_CUBE.height)
                 )
-                adUnitId = unitId
-                loadAd(AdRequest.Builder().build())
+            }
+        },
+        update = {adManagerView ->
+            adManagerView.adUnitId = unitId
+            adManagerView.doOnLayout {
+                adManagerView.loadAd(AdRequest.Builder().build())
             }
         }
     )
