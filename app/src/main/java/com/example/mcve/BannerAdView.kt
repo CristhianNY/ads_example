7464b7236c406740d3a7888a63f20e107a7ad102
cristhian <cristhian.espinosa@dowjones.com>
2023-05-31 09:15:40 -0500

    update adunit

--- app/src/main/java/com/example/mcve/BannerAdView.kt
+++ app/src/main/java/com/example/mcve/BannerAdView.kt
@@ -1,11 +1,14 @@
 package com.example.mcve
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.doOnLayout
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.admanager.AdManagerAdView
 
 @SuppressLint("VisibleForTests")
@@ -13,7 +16,7 @@ import com.google.android.gms.ads.admanager.AdManagerAdView
 fun BannerAdView(
     isTest: Boolean = false
 ) {
-    val unitId = "/2/androidphone.us.wsj.app/collection"
+    val unitId = "/2/androidphone.us.wsj.app/markets"
 
     AndroidView(
         factory = { context ->
@@ -22,13 +25,43 @@ fun BannerAdView(
                     AdSize(AdType.CUBE.width, AdType.CUBE.height),
                     AdSize(AdType.BIG_CUBE.width, AdType.BIG_CUBE.height)
                 )
+
+                adListener = object : AdListener() {
+                    override fun onAdLoaded() {
+                        // Este método se llama cuando un anuncio es recibido correctamente.
+                        Log.d("AdListener", "El anuncio ha sido cargado correctamente.")
+                        this@apply.resume()
+                    }
+
+                    override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                        // Este método se llama cuando un anuncio no se puede cargar.
+                        Log.d("AdListener", "El anuncio falló al cargarse: ${loadAdError.message}")
+                    }
+
+                    override fun onAdOpened() {
+                        // Este método se llama cuando el anuncio es clickeado y se abre el anuncio.
+                        Log.d("AdListener", "El anuncio ha sido abierto.")
+                    }
+
+                    override fun onAdClicked() {
+                        // Este método se llama cuando el anuncio es clickeado.
+                        Log.d("AdListener", "El anuncio ha sido clickeado.")
+                    }
+
+                    override fun onAdClosed() {
+                        // Este método se llama cuando el anuncio se cierra.
+                        Log.d("AdListener", "El anuncio ha sido cerrado.")
+                    }
+                }
             }
         },
-        update = {adManagerView ->
+        update = { adManagerView ->
             adManagerView.adUnitId = unitId
             adManagerView.doOnLayout {
+                // Realiza la carga del anuncio después de la pasada de diseño.
                 adManagerView.loadAd(AdRequest.Builder().build())
             }
         }
+
     )
 }
